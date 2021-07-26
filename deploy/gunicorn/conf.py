a55f5e45ed18f7686fc2196f87e0cea1337df5bc
panhq <hypofiasco@gmail.com>
2021-07-26 17:32:05 +0800

    improve gunicorn configuration

--- deploy/gunicorn/conf.py
+++ deploy/gunicorn/conf.py
@@ -5,7 +5,6 @@ debug = settings.DEBUG
 reload = debug
 workers = settings.WORKERS if not debug else 1
 worker_class = 'uvicorn.workers.UvicornWorker'
-access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
 # Gunicorn config variables
 bind = "0.0.0.0:8000"
