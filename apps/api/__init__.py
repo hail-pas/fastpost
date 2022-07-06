35f36630ae6081afba6b6541c6d86191e18a4572
panhq <hypofiasco@gmail.com>
2022-07-06 15:41:09 +0800

    add index route

--- apps/api/__init__.py
+++ apps/api/__init__.py
@@ -5,6 +5,7 @@ from fastapi import Depends, APIRouter
 
 from apps.api.routes import auth, user, enums, common
 from apps.dependencies import jwt_required
+from core.response import Resp
 
 api_router = APIRouter(prefix="")
 
@@ -12,3 +13,8 @@ api_router.include_router(auth.router, prefix="/auth", tags=["授权相关"])
 api_router.include_router(user.router, prefix="/user", tags=["用户信息管理"], dependencies=[Depends(jwt_required)])
 api_router.include_router(enums.router, prefix="/enums", tags=["码表映射"])
 api_router.include_router(common.router, prefix="/other", tags=["其他"])
+
+
+@api_router.get("/")
+async def index():
+    return Resp()
